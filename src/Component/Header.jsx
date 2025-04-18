8619ad1414e47846648de969605f5edf247db579
Tepan223 <stevanusgabrielbb34@gmail.com>
2025-04-19 01:25:51 +0700

    Responsive

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -7,6 +7,12 @@ function Header () {
                 <div className='title pc' >Stevanus Gabriel</div>
                 <div className='navigation pc'>
                     <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#portfolio">Portfolio</a>
+                    <a href="#contact">Contact</a>
+                </div>
+                <div className='navigation mb'>
+                    <a href="#home"><img src="Home.svg" alt="" className='logo'/></a>
                     <a href="#about">About & Skill</a>
                     <a href="#portfolio">Portfolio</a>
                     <a href="#contact">Contact</a>
