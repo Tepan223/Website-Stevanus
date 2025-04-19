90d37cc37d7db7ad55e57464fff734754ca119a8
Tepan223 <stevanusgabrielbb34@gmail.com>
2025-04-20 00:39:05 +0700

    New

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -14,9 +14,35 @@ function Home () {
                         <p className='description-home'>I am a Web Developer</p>
                     </div>
                     <div className='content-contact-home'>
-
+                        <div className='content-icon-home-whatsapp'>
+                            <a href="" className='logo-img-home'>
+                                <img src="Whatsapp.svg" alt="Whatsapp" className='img-contact-home'/>
+                            </a>
+                        </div>
+                        <div className='content-icon-home-instagram'>
+                            <a href="" className='logo-img-home'>
+                                <img src="Instagram.svg" alt="Instagram" className='img-contact-home'/>
+                            </a>
+                        </div>
+                        <div className='content-icon-home-tiktok'>
+                            <a href="" className='logo-img-home'>
+                                <img src="TikTok.svg" alt="TikTok" className='img-contact-home'/>
+                            </a>
+                        </div>
+                        <div className='content-icon-home-email'>
+                            <a href="" className='logo-img-home'>
+                                <img src="Email.svg" alt="Email" className='img-contact-home'/>
+                            </a>
+                        </div>
+                        <div className='content-icon-home-github'>
+                            <a href="" className='logo-img-home'>
+                                <img src="Github.svg" alt="GitHub" className='img-contact-home'/>
+                            </a>
+                        </div>
                     </div>
-                    <div className='content-btn-home'></div>
+                    <div className='content-btn-home'>
+                        <button className='btn-home'>More About Me</button>
+                    </div>
                 </div>
                 <div className='content-right-home'></div>
             </div>
