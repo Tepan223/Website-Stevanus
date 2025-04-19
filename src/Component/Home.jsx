4b535ad287f5604377636a86c41eb9916c4fd6a8
Tepan223 <stevanusgabrielbb34@gmail.com>
2025-04-19 23:33:21 +0700

    new

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,7 +3,23 @@ function Home () {
     return (
         <>
         <div className='home' id='home'>
-            
+            <div className='content-home'>
+                <div className='content-left-home'>
+                    <div className='content-title-home'>
+                        <h1 className='title1-home'>Hello, It`s Me</h1>
+                        <h1 className='title2-home'>Stevanus Gabriel</h1>
+                        <h1 className='title3-home'>And I am a Web Developer</h1>
+                    </div>
+                    <div className='content-description-home'>
+                        <p className='description-home'>I am a Web Developer</p>
+                    </div>
+                    <div className='content-contact-home'>
+
+                    </div>
+                    <div className='content-btn-home'></div>
+                </div>
+                <div className='content-right-home'></div>
+            </div>
         </div>
         </>
     )
